3723e59c892cf67224666f266bf69eb2a98e4130
= <=>
2018-06-12 17:05:20 +0530

    Removed folder

--- Atria_DataVision_Automation/ProtractorFramework/testData/securityQuestions.ts
+++
@@ -1,8 +0,0 @@
-
-
-
-export var securityQuestions = {
-    "How do you spell \"password\"?" : "password",
-    "What is 2+2?":"4",
-
-}
--- ProtractorFramework/out/testData/securityQuestions.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.securityQuestions = {
-    "How do you spell \"password\"?": "password",
-    "What is 2+2?": "4",
-};
