e7276ed194930fd6d7a70ab3a2e895ef0cf397d2
Prajyot Fulsundar <38970432+prajyotfulsundar@users.noreply.github.com>
2019-11-21 10:51:57 +0530

    ax

--- ProtractorFramework/testData/contactsPhone.ts
+++
@@ -1,9 +0,0 @@
-export var contactsPhone = {
-  "Brian Fernandes": "Primary cell",
-  "Hrushikesh Bawachkar": "Primary cell",
-  "Classic Dev23": "Primary cell",
-  "Classic Dev16": "Primary cell",
-  "Mukesh Vijayababu": "Primary cell",
-  "Classic Dev26": "Primary cell",
-  "Classic Dev3": "Primary cell"
-};
